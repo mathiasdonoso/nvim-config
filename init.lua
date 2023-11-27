2bc28b95e51d948b7a20e952bc2f7ccf29acf44b
Mathías Donoso <mathiasd88@gmail.com>
2023-11-27 01:54:10 -0300

    feat: added options for identation

--- init.lua
+++ init.lua
@@ -204,6 +204,12 @@ vim.o.hlsearch = false
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
