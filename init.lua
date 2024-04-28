ca619073f48acc5d6a8c9f3289d22a7d02d62c4d
Mathias <mathiasd88@gmail.com>
2024-04-28 13:36:47 -0400

    feat: adding missing and very important undotree command

--- init.lua
+++ init.lua
@@ -74,6 +74,7 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "Q", "<Nop>", { silent = true })
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Exp)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
