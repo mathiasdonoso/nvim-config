ad1d52e0fbae4c73c9c49b53a9e7808c83eab480
Mathias Donoso <mathiasd88@gmail.com>
2024-05-06 00:45:44 -0400

    feat: removing tab of len 4

--- lua/m/set.lua
+++ lua/m/set.lua
@@ -28,9 +28,9 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
