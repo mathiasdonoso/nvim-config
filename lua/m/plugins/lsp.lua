6fb76f5da20aa178dec456272905db8df296d146
Mathias <mathiasd88@gmail.com>
2024-07-03 21:08:31 -0400

    changing some config

--- lua/m/plugins/lsp.lua
+++ lua/m/plugins/lsp.lua
@@ -67,7 +67,7 @@ return {
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
@@ -151,7 +151,7 @@ return {
 			local servers = {
 				clangd = {},
 				gopls = {},
-				pyright = {},
+				-- pyright = {},
 				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
