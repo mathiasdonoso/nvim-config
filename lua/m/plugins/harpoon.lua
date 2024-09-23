6062e5ca5c14dbdeaa5505d8ac7343b9a30249ab
Mathias <mathiasd88@gmail.com>
2024-09-22 22:07:02 -0300

    feat: Stealing tj config for harpoon

--- lua/m/plugins/harpoon.lua
+++ lua/m/plugins/harpoon.lua
@@ -16,18 +16,11 @@ return {
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', '<S-q>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<S-w>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<S-e>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<S-r>', function()
-      harpoon:list():select(4)
-    end)
+    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+      vim.keymap.set('n', string.format('<space>%d', idx), function()
+        harpoon:list():select(idx)
+      end)
+    end
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
